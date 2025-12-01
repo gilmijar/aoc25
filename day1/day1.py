63514abbd69aad5a3bda8a1136c32459c97eff27
Ilia Gilmijarow <gilmijar@gmail.com>
2025-12-01 17:52:11 +0100

    Add initial project files and implementation for day 1 challenge p1

---
+++ day1/day1.py
@@ -0,0 +1,36 @@
+file_name = 'sample.txt'
+# file_name = 'input.txt'
+
+datafile = open(file_name, 'r', encoding='UTF-8').read().strip().splitlines()
+
+DIAL = 100
+
+def parse(lines:list[str]) -> list[int]:
+    return [ (-1 if line.startswith('L') else 1 ) * int(line[1:]) for line in lines ]
+
+def land_on_zero(commands:list[int]) -> list[int]:
+    pos = 50
+    all_pos = [pos]
+    for cmd in commands:
+        pos = (pos + cmd) % DIAL
+        all_pos.append(pos)
+    return all_pos
+
+print(land_on_zero(parse(datafile)).count(0))
+
+# PART 2
+
+def zero_cosses(commands:list[int]) -> int:
+    pos = 50
+    zeros = 0
+    for cmd in commands:
+        x = pos + cmd
+        pos = x % DIAL
+        print(pos)
+        if x > 99 or x < 1:
+            zeros += 1
+    return zeros
+
+print(
+    zero_cosses(parse(datafile))
+)
