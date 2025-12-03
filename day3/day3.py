cfbf550cdcf0b1ba94510fb10fcf58102607158d
Ilia Gilmijarow <gilmijar@gmail.com>
2025-12-03 09:48:02 +0100

    day3 part 1 and 2 done

---
+++ day3/day3.py
@@ -0,0 +1,18 @@
+file_name = 'sample.txt'
+file_name = 'input.txt'
+
+datafile = open(file_name, 'r', encoding='UTF-8').read().strip().splitlines()
+
+def find_max_jolts(line:str)->int:
+    max_digit = max(line[:-1])
+    max_index = line.index(max_digit)
+    rest = line[max_index+1:]
+
+    second_digit = max(rest)
+
+    return int(max_digit + second_digit)
+
+
+total = sum( find_max_jolts(line) for line in datafile)
+
+print(total)
---
+++ day3/day3_2.py
@@ -0,0 +1,21 @@
+file_name = 'sample.txt'
+file_name = 'input.txt'
+
+datafile = open(file_name, 'r', encoding='UTF-8').read().strip().splitlines()
+
+def find_max_jolts(line:str)->int:
+    digits:list[str] = []
+    for x in range(-11, 0, 1):
+        max_digit = max(line[:x])
+        digits.append(max_digit)
+        line = line.partition(max_digit)[2]
+    last_digit = max(line)
+    digits.append(last_digit)
+    result = ''.join(digits)
+    # print('=>', result)
+    return int(result)
+ 
+
+total = sum( find_max_jolts(line) for line in datafile)
+
+print(total)
