2f3c3176c8b88d97f9db9c2b62b85a3592edbaec
Ilia Gilmijarow <gilmijar@gmail.com>
2025-12-05 12:52:03 +0100

    day5 part1 done

---
+++ day05/day5_p1.py
@@ -0,0 +1,30 @@
+from os import chdir
+chdir(__file__.rpartition("\\")[0])
+
+file_name = "sample.txt"
+file_name = 'input.txt'
+
+datafile = open(file_name, "r", encoding="UTF-8").read().strip().splitlines()
+
+empty_line = datafile.index("")
+s_ranges = datafile[:empty_line]
+s_items = datafile[empty_line+1:]
+
+def parse_range(rng: str)->tuple[int, int]:
+    a, b, *_ = map(int, rng.split('-'))
+    return a, b+1
+
+ranges = tuple(range(*parse_range(r)) for r in s_ranges)
+items = tuple(int(x) for x in s_items)
+
+# check
+fresh: list[int] = []
+for item in items:
+    for range in ranges:
+        '''QUADRATIC!!!!!! YEAH!'''
+        if item in range:
+            fresh.append(item)
+            break
+
+print(fresh)
+print(len(fresh))
